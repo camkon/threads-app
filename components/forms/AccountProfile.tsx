fc1db01ca21ae319a0e291e93ecc7b47c825c801
Ansaf Nisam <80558066+camkon@users.noreply.github.com>
2024-01-12 06:44:01 +0530

    feat : onboarding complete
    # mongodb setup
    # onboarding setup
    # backend setup

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import React from "react"
+import * as z from "zod"
+import Image from "next/image"
 import { useForm } from "react-hook-form"
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
@@ -8,8 +10,10 @@ import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { userValidation } from "@/lib/validations/user"
-import * as z from "zod"
-import Image from "next/image"
+import { isBase64Image } from "@/lib/utils"
+import { useUploadThing } from "@/lib/uploadthing"
+import { updateUser } from "@/lib/actions/user.actions"
+import { usePathname, useRouter } from "next/navigation"
 
 interface Props {
   user: {
@@ -25,22 +29,68 @@ interface Props {
 
 const AccountProfile = ({user, buttonTitle}: Props) => {
 
+  const [files, setFiles] = React.useState<File[]>([])
+
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const { startUpload } = useUploadThing("media")
+
   const form = useForm({
     resolver: zodResolver(userValidation),
     defaultValues: {
-      profile_photo: "",
-      name: "",
-      username: "",
-      bio: "",
+      profile_photo: user?.image || "",
+      name: user?.name || "",
+      username: user?.username || "",
+      bio: user?.bio || "",
     },
   })
 
-  const handleImage = (e:React.ChangeEvent, fieldChange:(value: string) => void) => {
+  const handleImage = (e:React.ChangeEvent<HTMLInputElement>, fieldChange:(value: string) => void) => {
     e.preventDefault()
+    const fileReader = new FileReader()
+
+    if(e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0]
+      setFiles(Array.from(e.target.files))
+      if(!file.type.includes("image")) return;
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event?.target?.result?.toString() || ""
+        fieldChange(imageDataUrl)
+      }
+
+      fileReader.readAsDataURL(file)
+    }
   }
 
-  function onSubmit(values: z.infer<typeof userValidation>) {
-    console.log(values)
+  const onSubmit = async (values: z.infer<typeof userValidation>) => {
+
+    const blob = values.profile_photo
+    const hasImageChanged = isBase64Image(blob)
+
+    if(hasImageChanged) {
+      const imgRes = await startUpload(files)
+
+      if(imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl
+      }
+    }
+
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,    
+    })
+
+    if(pathname === '/profile/edit') {
+      router.back()
+    }else {
+      router.push('/')
+    }
   }
 
   return(
